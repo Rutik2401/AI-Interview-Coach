0bec5873596e1605fda294b7e652181a15cf0ea3
rutikpimpale2401 <rutikpimpale2401@gmail.com>
2025-06-21 13:03:27 +0530

    Project Structure Created

---
+++ ai-interview-coach/src/app/components/interview/interview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview',
+  imports: [],
+  standalone: true,
+  templateUrl: './interview.component.html',
+  styleUrl: './interview.component.scss'
+})
+export class InterviewComponent {
+
+}
